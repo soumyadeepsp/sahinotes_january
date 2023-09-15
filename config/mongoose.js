b16000eb761de6d3c2cec036e96bcecbd6ecbb0c
soumyadeepsp <soumyadeepsp@gmail.com>
2023-09-15 21:30:53 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://18.119.130.95:27017/sahinotes'; // Replace with your MongoDB URI
+const dbURI = 'mongodb://127.0.0.1:27017/sahinotes'; // Replace with your MongoDB URI
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
